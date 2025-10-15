2d9f6434c1b481248a14b3d3717de897881f7968
Amisha Sinha <amisha@iastate.edu>
2025-10-15 14:35:53 -0500

    Add comment about exit status in CheckCommandSuccess.sh
    Added a comment explaining the exit status of the last command.

--- Linux/Day1/CheckCommandSuccess.sh
+++ Linux/Day1/CheckCommandSuccess.sh
@@ -7,3 +7,4 @@ else
   echo "Command failed"
   exit 1
 fi
+#$? is the exit status of the last executed command (which was ls /tmp). If the exit code of the ls /tmp command is 0 (i.e., it succeeded), then execute the following block
