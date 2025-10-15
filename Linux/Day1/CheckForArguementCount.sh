e4f41636cea3b6bd2c98bb0e9427d0ed82c43914
Amisha Sinha <amisha@iastate.edu>
2025-10-15 14:34:31 -0500

    Add comment for argument count explanation
    Added a comment explaining the special variable for argument count.

--- Linux/Day1/CheckForArguementCount.sh
+++ Linux/Day1/CheckForArguementCount.sh
@@ -6,3 +6,5 @@ if [[ $# -lt 1 ]]; then
 else
   echo "Argument provided: $1"
 fi
+
+#  $#: This is a special variable that holds the Number of command-line arguments passed to the script.If the number of arguments ($#) is less than 1 (meaning zero arguments), then run the following block.
